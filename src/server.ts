b89e255ff264381dded549be448e5e757356bdf2
Wesley <wesmilitao@gmail.com>
2023-05-29 17:17:04 -0300

    server changes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { SpellsRoute } from "./routes/spells";
 import cors from "@fastify/cors";
+import "dotenv/config";
 
 const app = fastify();
 
@@ -9,6 +10,6 @@ app.register(cors, {
   origin: true,
 });
 
-app.listen({ host: "0.0.0.0", port: 3333 }, () =>
-  console.log("server is running")
-);
+app.listen({ host: "0.0.0.0", port: Number(process.env.PORT) || 3333 }, () => {
+  console.log("server is running");
+});
